d274e5aa53c43ecbc6e5748891a2fd0bfcde702d
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-07-26 16:58:51 +0200

    Added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
 };
