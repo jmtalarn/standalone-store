7d984ba468dd81e03919fd521bfc39f8f63e745a
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-07-29 11:48:15 +0200

    Solved issues with some webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const env = process.env.NODE_ENV || "development";
-const isDev = env === "development";
-const isProd = env === "production";
+const env = process.env.NODE_ENV || 'development';
+const isDev = env === 'development';
+const isProd = env === 'production';
 
 const extractCss = new ExtractTextPlugin({
-  filename: "index.css",
+  filename: 'index.css',
   disable: isDev,
 });
 
 module.exports = {
   entry: {
-    bundle: "./src/index.js",
+    bundle: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   plugins: [new HtmlWebpackPlugin(), extractCss],
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "eslint-loader",
+        use: 'eslint-loader',
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: extractCss.extract({
-          use: [{ loader: "css-loader" }],
-          fallback: "style-loader",
+          use: [{ loader: 'css-loader' }],
+          fallback: 'style-loader',
         }),
       },
     ],
